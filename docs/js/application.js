bac9b3cfeb87515659faf6b38572ad8e12eb4ee2
Jason Golieb <jgolieb@hortonworks.com>
2018-10-12 16:47:25 -0400

    Cross-browser scrolling via CSS and fixed menu positioning via JS

--- docs/js/application.js
+++ docs/js/application.js
@@ -6,25 +6,47 @@
  * of all or any part of the contents of this file is strictly prohibited.
  */
 
+const positionMenus = () => {
+  // Helper method for getting absolute top position
+  const offsetTop = el => {
+    if (el) {
+      return el.offsetTop + offsetTop(el.offsetParent);
+    }
+
+    return 0;
+  }
+
+  //$('.navbar .nav-item.dropdown.show').dropdown('toggle'); //close all dropdowns
+
+  // Position dropdown menus
+  $('.navbar .nav-item.dropdown .dropdown-menu').each((i, el) => {
+    el.style.top = `${offsetTop(el.parentElement) - el.parentElement.offsetParent.scrollTop + 36}px`;
+  });
+
+  // Position sub-item flyout menus
+  $('.navbar-nav.collapse').each((i, el) => {
+    const dataTarget = el.id;
+    const parentElement = $(`.navbar-nav[data-toggle="collapse"][data-target="#${dataTarget}"]`)[0];
+    if (parentElement) {
+      el.style.top = `${offsetTop(parentElement) - parentElement.offsetParent.scrollTop}px`;
+    }
+  });
+}
+
+const preventClickEventsWhenCollapsed = event => {
+  const navbar = $(event.currentTarget).closest('.navbar.collapse')[0];
+  if (!$(navbar).hasClass('show')) {
+    event.stopImmediatePropagation();
+  }
+}
+
 $(function () {
   $('[data-toggle="popover"]').popover();
   $('[data-toggle="tooltip"]').tooltip();
-  $('.navbar').on('hidden.bs.collapse', function (event) {
-    if (!event.target.classList.contains("brand-menu") // exclude the brand menu from this behavior
-      && event.target === event.currentTarget) {
-      $(this).find('.navbar-nav.collapse').css('margin-bottom', function () {
-        return `${this.children.length * -38}px`;
-      });
-    }
-  });
-  $('.navbar').on('shown.bs.collapse', function (event) {
-    if (!event.target.classList.contains("brand-menu") // exclude the brand menu from this behavior
-      && event.target === event.currentTarget) {
-      $(this).find('.navbar-nav.collapse').css('margin-bottom', '0');
-    }
-  });
-  $('.navbar-collapse').on('wheel', function (event) {
-    event.preventDefault();
-    event.currentTarget.scrollTop += event.originalEvent.deltaY;
-  });
+
+  positionMenus();
+  $('.navbar-collapse').scroll(positionMenus);
+
+  $('.navbar.collapse .navbar-nav[data-toggle="collapse"]').click(preventClickEventsWhenCollapsed);
+  $('.navbar.collapse .nav-item.dropdown .dropdown-toggle').click(preventClickEventsWhenCollapsed);
 });
