daf84cf5bcb0bd5b677555f998324be9d1d32b26
Jason Golieb <j@golieb.net>
2018-11-27 13:16:04 -0500

    Build script and file layout changes (#22)
    * Make tweaks from feedback and for browser compatibility
    
    * Make more browser compatibility tweaks
    
    * Implement wheel scrolling; make js build task consistent
    
    * Cross-browser scrolling via CSS and fixed menu positioning via JS
    
    * Add text-overflow: ellipsis behavior
    
    * Implement pagination control; update color system
    
    * Remove $fluid- prefix from navbar colors
    
    * Adjust navbar top and bottom padding for presence of navbar-brand and/or navbar-toggler
    
    * Implement responsive container and some miscellaneous changes
    
    * Add header component
    
    * Make navbar IE compatible
    
    * Update to Bootstrap@4.1.3 and stop using CDN
    
    * More IE compatibility changes
    
    * Fix bug in previous commit
    
    * Enable ellipsis overflow for submenu items in collapsed state
    
    * Fix merge bug
    
    * Fix file structure merge issues
    
    * Align file structure with Bootstrap and improve build process
    
    * Improve containers and responsive layout
    
    * Improve CSS build task
    
    * Make build task parallel
    
    * Signal async completion of gulp tasks
    
    * Fix live reload
    
    * Fix bug in merge
    
    * Fix bug in merge
    
    * Fix bug in merge

--- gulpfile.js
+++ gulpfile.js
@@ -5,64 +5,60 @@
  * modification, redistribution, sharing, lending or other exploitation
  * of all or any part of the contents of this file is strictly prohibited.
  */
-
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var cleanCss = require('gulp-clean-css');
-var rename = require('gulp-rename');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var bulkSass = require('gulp-sass-glob-import');
-var del = require('del');
-var connect = require('gulp-connect');
-var babel = require("gulp-babel");
-
-// sources
-var fonts_src = './node_modules/font-awesome/fonts/fontawesome-webfont.*';
-var sass_src = [
-  './src/**/*.scss',
-  './node_modules/font-awesome/scss/*.scss'
-];
-var sass_docs_src = './application.scss';
-var html_src = './*.html';
-var js_src = './application.js';
-var js_docs_src = [
-  './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
-  './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map'
-];
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const cleanCss = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const bulkSass = require('gulp-sass-glob-import');
+const del = require('del');
+const connect = require('gulp-connect');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
 
 // destinations
-var html_dest = './docs/';
-var js_dest = './dist/scripts/';
-var js_docs_dest = './docs/scripts/';
-var fonts_dest = './dist/fonts/';
-var fonts_docs_dest = './docs/fonts/';
-var css_dest = './dist/styles/';
-var css_docs_dest = './docs/styles/';
+const fonts_dest = './dist/fonts/';
+const fonts_docs_dest = './docs/fonts/';
 
-gulp.task('copy-html', () =>
-  gulp.src(html_src)
-    .pipe(gulp.dest(html_dest))
-    .pipe(connect.reload())
-);
+const html_dest = './docs/';
 
-gulp.task('build-js', () =>
-  gulp.src(js_src)
-    .pipe(sourcemaps.init())
-    .pipe(babel())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(js_dest))
-    .pipe(gulp.dest(js_docs_dest))
-    .pipe(connect.reload())
-);
+const css_dest = './dist/css/';
+const css_docs_dest = './docs/css/';
 
-gulp.task('copy-docs-js', () =>
-  gulp.src(js_docs_src)
-    .pipe(gulp.dest(js_docs_dest))
-    .pipe(connect.reload())
-);
+const js_build_dest = './js/dist/';
+const js_bundle_dest = './dist/js/';
+const js_bundle_name = 'fluid-bootstrap.js';
+const js_docs_dest = './docs/js/';
 
+
+// sources
+const fonts_src = './node_modules/font-awesome/fonts/fontawesome-webfont.*';
+
+const html_src = './*.html';
+
+const sass_src = [
+  './scss/**/*.scss',
+  './node_modules/font-awesome/scss/*.scss'
+];
+const css_src = [
+  css_dest + '*.css',
+  '!' + css_dest + '*.min.css'
+];
+const sass_docs_src = './demo.scss';
+
+const js_build_src = './js/src/*.js';
+const js_bundle_src = [
+  './node_modules/bootstrap/dist/js/bootstrap.bundle.js',
+  js_build_dest + '*.js'
+];
+const js_minify_src = js_bundle_dest + js_bundle_name;
+const js_docs_src = './demo.js';
+
+
+// tasks
 gulp.task('copy-fonts', () =>
   gulp.src(fonts_src)
     .pipe(gulp.dest(fonts_dest))
@@ -70,51 +66,104 @@ gulp.task('copy-fonts', () =>
     .pipe(connect.reload())
 );
 
-gulp.task('build-sass', () =>
+gulp.task('copy-html', () =>
+  gulp.src(html_src)
+    .pipe(gulp.dest(html_dest))
+    .pipe(connect.reload())
+);
+
+gulp.task('transpile-sass', () =>
   gulp.src(sass_src)
     .pipe(bulkSass())
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([ autoprefixer({ browsers: ['Chrome >= 35', 'Firefox >= 38', 'Edge >= 12', 'Explorer >= 10', 'iOS >= 8', 'Safari >= 8', 'Android 2.3', 'Android >= 4', 'Opera >= 12']})]))
-    .pipe(sourcemaps.write())
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest(css_dest))
     .pipe(gulp.dest(css_docs_dest))
+);
+
+gulp.task('minify-css', () =>
+  gulp.src(css_src)
+    .pipe(sourcemaps.init())
     .pipe(cleanCss())
-    .pipe(rename({suffix: '.min'}))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest(css_dest))
     .pipe(gulp.dest(css_docs_dest))
     .pipe(connect.reload())
 );
+
+gulp.task('build-sass', gulp.series('transpile-sass', 'minify-css'));
 
 gulp.task('build-docs-sass', () =>
   gulp.src(sass_docs_src)
     .pipe(bulkSass())
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    .pipe(postcss([ autoprefixer({ browsers: ['Chrome >= 35', 'Firefox >= 38', 'Edge >= 12', 'Explorer >= 10', 'iOS >= 8', 'Safari >= 8', 'Android 2.3', 'Android >= 4', 'Opera >= 12']})]))
+    .pipe(postcss([autoprefixer({ browsers: ['Chrome >= 35', 'Firefox >= 38', 'Edge >= 12', 'Explorer >= 10', 'iOS >= 8', 'Safari >= 8', 'Android 2.3', 'Android >= 4', 'Opera >= 12'] })]))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(css_docs_dest))
+);
+
+gulp.task('transpile-js', () =>
+  gulp.src(js_build_src)
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(js_build_dest))
+);
+
+gulp.task('bundle-js', () =>
+  gulp.src(js_bundle_src)
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(concat(js_bundle_name))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(js_bundle_dest))
+    .pipe(gulp.dest(js_docs_dest))
+);
+
+gulp.task('minify-js', () =>
+  gulp.src(js_minify_src)
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(js_bundle_dest))
     .pipe(connect.reload())
 );
 
-gulp.task('clean', () => del(['./dist', './docs']));
+gulp.task('build-js', gulp.series('transpile-js', 'bundle-js', 'minify-js'));
 
-gulp.task('build', gulp.series('build-sass', 'build-docs-sass', 'build-js', 'copy-docs-js', 'copy-fonts', 'copy-html'));
+gulp.task('build-docs-js', () =>
+  gulp.src(js_docs_src)
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(js_docs_dest))
+);
 
-gulp.task('watch-sass', () => gulp.watch(sass_src, { ignoreInitial: false }, gulp.series('build-sass')));
-gulp.task('watch-docs-sass', () => gulp.watch(sass_docs_src, { ignoreInitial: false }, gulp.series('build-docs-sass')));
-gulp.task('watch-js', () => gulp.watch(js_src, { ignoreInitial: false }, gulp.series('build-js')));
-gulp.task('watch-docs-js', () => gulp.watch(js_docs_src, { ignoreInitial: false }, gulp.series('copy-docs-js')));
-gulp.task('watch-fonts', () => gulp.watch(fonts_src, { ignoreInitial: false }, gulp.series('copy-fonts')));
-gulp.task('watch-html', () => gulp.watch(html_src, { ignoreInitial: false }, gulp.series('copy-html')));
+gulp.task('clean', () => del(['./dist', './docs', js_build_dest]));
 
-gulp.task('watch', gulp.parallel('watch-sass', 'watch-docs-sass', 'watch-fonts', 'watch-html', 'watch-js', 'watch-docs-js'));
+gulp.task('build', gulp.parallel('copy-fonts', 'copy-html', 'build-sass', 'build-docs-sass', 'build-js', 'build-docs-js'));
 
-gulp.task('connect', () => connect.server({
+gulp.task('connect', done => {
+  connect.server({
     root: 'docs',
     port: 8000,
     livereload: true
-  })
-);
+  });
 
-gulp.task('default', gulp.parallel('watch', 'connect'));
+  done();
+});
+
+gulp.task('watch-fonts', () => gulp.watch(fonts_src, { ignoreInitial: false }, gulp.series('copy-fonts')));
+gulp.task('watch-html', () => gulp.watch(html_src, { ignoreInitial: false }, gulp.series('copy-html')));
+gulp.task('watch-sass', () => gulp.watch(sass_src, { ignoreInitial: false }, gulp.series('build-sass')));
+gulp.task('watch-docs-sass', () => gulp.watch(sass_docs_src, { ignoreInitial: false }, gulp.series('build-docs-sass')));
+gulp.task('watch-js', () => gulp.watch(js_build_src, { ignoreInitial: false }, gulp.series('build-js')));
+gulp.task('watch-docs-js', () => gulp.watch(js_docs_src, { ignoreInitial: false }, gulp.series('build-docs-js')));
+
+gulp.task('watch', gulp.parallel('watch-fonts', 'watch-html', 'watch-sass', 'watch-docs-sass', 'watch-js', 'watch-docs-js'));
+
+gulp.task('default', gulp.series('clean', 'connect', 'watch'));
